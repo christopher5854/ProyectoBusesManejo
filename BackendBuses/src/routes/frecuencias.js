d462d6b87199711e224dc4e7407944c78a162903
The-Fabry <andrestoasa93@gmail.com>
2026-05-14 15:07:45 -0500

    feat(routes): agregar rutas de paradas intermedias (listar, agregar, eliminar) [BUS-16]

--- BackendBuses/src/routes/frecuencias.js
+++ BackendBuses/src/routes/frecuencias.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const frecuenciaController = require('../controllers/frecuenciaController');
+const paradaController = require('../controllers/paradaController');
 
 // Rutas de Frecuencias
 router.get('/', frecuenciaController.listarFrecuencias);
 router.post('/', frecuenciaController.crearFrecuencia);
 router.put('/:id', frecuenciaController.actualizarFrecuencia);
 router.get('/buscar', frecuenciaController.buscarFrecuencias);
+
+// Rutas de Paradas Intermedias
+router.get('/:frecuenciaId/paradas', paradaController.listarParadas);
+router.post('/:frecuenciaId/paradas', paradaController.agregarParada);
+router.delete('/:frecuenciaId/paradas/:paradaId', paradaController.eliminarParada);
