1cc0e21bd9b90170273d6e3c14710d14d5ac2f34
The-Fabry <andrestoasa93@gmail.com>
2026-05-14 15:18:02 -0500

    feat(server): registrar endpoints de frecuencias, paradas, hojas y rutas en el servidor principal

--- BackendBuses/src/index.js
+++ BackendBuses/src/index.js
@@ -16,6 +16,10 @@ app.use('/api/usuarios',   require('./routes/usuarios'));
 app.use('/api/ciudades',   require('./routes/ciudades'));
 app.use('/api/auth',       require('./routes/auth.routes'));
 
+//Importación y uso de rutas de frecuencias
+const frecuenciasRoutes = require('./routes/frecuencias');
+app.use('/api/frecuencias', frecuenciasRoutes);
+
 app.get('/', (req, res) => {
   res.json({ mensaje: 'API TransiSys funcionando' });
 });
