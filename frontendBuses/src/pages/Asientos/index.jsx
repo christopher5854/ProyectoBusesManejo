e58533e3c88fd20c9c4d3ada34677e10ee7eb1fc
JUANPI <juanpi.10pc@gmail.com>
2026-05-18 23:27:00 -0500

    feat(asientos): crear estructura base de página de asientos

---
+++ frontendBuses/src/pages/Asientos/index.jsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
+export default function AsientosPage() {
+  return (
+    <Container maxWidth="sm" sx={{ mt: 4 }}>
+      <Typography variant="h5" fontWeight="bold" color="primary">
+        Selecciona tu asiento
+      </Typography>
+    </Container>
+  );
+}
